e681f4cc56e85e342692301ca4928d0120613e0b
DarkM1nd <125229600+VZNURUIM@users.noreply.github.com>
2023-11-26 13:21:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ app = Flask(__name__)
 # Set your OpenAI API key here
 openai.api_key = "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"
 
-# Memory to store user inputs and assistant responses
 memory = []
 
 def ask_assistant(question):
@@ -32,18 +31,15 @@ def assistant_interaction():
         data = request.get_json()
         user_input = data['pergunta']
 
-        # Check if the user wants the assistant to repeat the previous response
         if user_input.lower() in ['repeat', 'repita', 'repetir']:
             if memory:
-                response = memory[-1]  # Retrieve the last response from memory
+                response = memory[-1]  
             else:
                 response = "Desculpe, não há nada para repetir."
         else:
             response = ask_assistant(user_input)
-            # Store the current user input and assistant response in memory
             memory.append(response)
 
-        # Call the function to handle speech synthesis and display on the page
         threading.Thread(target=text_to_speech_and_display, args=(user_input, response)).start()
 
         return jsonify({'resposta': response})
@@ -57,10 +53,8 @@ def text_to_speech_and_display(user_input, bot_response):
     engine.say(bot_response)
     engine.runAndWait()
 
-    # Wait for 20 seconds before responding again
     time.sleep(20)
 
-    # Display the conversation in the chat-box with typing animation
     chat_box = f"<div><strong>Você:</strong> {user_input}</div><div><strong>Assistente Jarvis:</strong> </div>"
     for char in bot_response:
         time.sleep(0.05)  # Adjust typing speed here (smaller value for faster typing)
